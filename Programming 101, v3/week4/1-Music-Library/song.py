a99729ef319e62e54ba0f5b0b5ee6ad79e2b2b3f
Petar Ivanov <pepincho95@gmail.com>
2015-04-13 17:57:28 +0300

    Adding song test

--- Programming 101, v3/week4/1-Music-Library/song.py
+++ Programming 101, v3/week4/1-Music-Library/song.py
@@ -5,32 +5,60 @@ class Song:
         self.artist = artist
         self.album = album
         self.length = length
+        self.path = ''
 
     def __str__(self):
-        return "{} - {} from {} - {}".format(self.artist, self.title, self.album, self.length)
+        return "{} - {} from {} - {}"\
+            .format(self.artist, self.title, self.album, self.length)
 
-    def __eq__(self):
-        result = self.title == other.title and self.artist == other.artist and self.album == other.album and self.length == other.length
-        return result
+    # def __eq__(self, other):
+    #     result = self.title == other.title and self.artist == other.artist and self.album == other.album and self.length == other.length
+    #     return result
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return self.__str__()
 
     def __hash__(self):
-        to_hash_class = self.title + self.artist + self.album + self.length
-        return hash(to_hash_class)
+        return hash(self.__str__())
 
     def get_length(self, argument=''):
         length_list = self.length.split(':')[::-1]
-        # print (length_list)
-        if argument is 'seconds' and len(length_list) >= 0:
-            return int(length_list[0])
-        elif argument is 'minutes' and len(length_list) >= 1:
-            return int(length_list[1])
-        # elif argument is 'hours' and len(length_list) >= 2:
-        #     return int(length_list[2])
-        elif argument is '':
-            # return str(self.length)
-            return "{}".format(self.length)
+        seconds = 0
+        minutes = 0
+        hours = 0
+
+        if len(length_list) == 3:
+            seconds = int(length_list[0])
+            minutes = int(length_list[1])
+            hours = int(length_list[2])
+        elif len(length_list) == 2:
+            seconds = int(length_list[0])
+            minutes = int(length_list[1])
         else:
-            return 0
+            raise ValueError(
+                "Length not proper format: {}".format(self.length))
+
+        if argument is 'seconds':
+            return seconds + 60 * minutes + 60 * 60 * hours
+        if argument is 'minutes':
+            return minutes + 60 * hours
+        if argument is 'hours':
+            return hours
+
+        # if argument is 'seconds' and len(length_list) >= 0:
+        #     return int(length_list[0])
+        # elif argument is 'minutes' and len(length_list) >= 1:
+        #     return int(length_list[1])
+        # elif argument is 'hours' and len(length_list) >= 2:
+        # return int(length_list[2])
+        # elif argument is '':
+        # return str(self.length)
+        #     return "{}".format(self.length)
+        # else:
+        #     return 0
 
     def get_info(self):
         return [self.artist, self.title, self.length]
