1572d5420518dcd2388a3fe1d3125d8d9a91aea5
Petar Ivanov <pepincho95@gmail.com>
2015-05-03 19:45:56 +0300

    adding directed graph to Who follows you back on Github

---
+++ Programming 101, v3/week6/directed_graph_test.py
@@ -0,0 +1,15 @@
+import unittest
+from directed_graph import DirectedGraph
+
+
+class Test_DirectedGraph(unittest.TestCase):
+
+    def setUp(self):
+        self.graph = DirectedGraph()
+
+    def test_generate_graph(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
