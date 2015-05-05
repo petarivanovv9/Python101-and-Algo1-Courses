604d59784d60ed8f15154eeb197d427538db5cea
Petar Ivanov <pepincho95@gmail.com>
2015-05-05 12:49:19 +0300

    adding student's courses query

--- Programming 101, v3/week8/1-The-Last-HR/main.py
+++ Programming 101, v3/week8/1-The-Last-HR/main.py
@@ -1,10 +1,33 @@
 from create_hackbulgaria_database import CreateHackBulgariaDatabase
 import json
 import requests
+import sys
 
 # dekartovo proizvedenie
 # SELECT user_name, user_email, post_title FROM Users
 # JOIN Posts ON Users.user_id = Posts.author
+
+
+def list_courses(db):
+    courses = db.get_all_courses()
+
+    for course in courses:
+        print ("{} - {}".format(course["course_id"], course["course_name"]))
+
+
+def list_students(db):
+    students = db.get_all_students()
+
+    for student in students:
+        print (
+            "{} - {} - {}".format(student["student_id"], student["student_name"], student["student_github"]))
+
+
+def list_student_in_courses(db):
+    stundet_in_courses = db.get_student_in_courses()
+
+    for item in stundet_in_courses:
+        print ("{} - {}".format(item["student_name"], item["course_name"]))
 
 
 def main():
@@ -37,6 +60,33 @@ def main():
                 current_student_id, current_course_id, current_student_group)
 
     db.database.commit()
+
+    print ("Menu:")
+    print ("1. List all students with their GitHub accounts.")
+    print ("2. List all courses.")
+    print (
+        "3. List all students and for each student - list the courses he has been attending.")
+    print (
+        "4. Find the students that have attented the most courses in Hack Bulgaria.")
+    print ("0. Exit.")
+
+    while True:
+        user_command = int(input("command: "))
+
+        if user_command == 1:
+            list_students(db)
+        elif user_command == 2:
+            list_courses(db)
+        elif user_command == 3:
+            # pass
+            list_student_in_courses(db)
+        elif user_command == 4:
+            pass
+        elif user_command == 0:
+            sys.exit()
+        else:
+            print ("Invalid command!")
+
 
 if __name__ == '__main__':
     main()
