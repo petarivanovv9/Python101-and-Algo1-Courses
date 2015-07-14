20211927e83e869bae95f7098492e67eb050cc57
Petar Ivanov <pepincho95@gmail.com>
2015-07-14 17:27:15 +0300

    add roots, heap_sort and k_min tasks to week2

--- Algo-1/week2/7-K-Min/k_min.cpp
+++ Algo-1/week2/7-K-Min/k_min.cpp
@@ -1,12 +1,46 @@
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
-using namespace std;
 
 class KMin {
 public:
+	// Finds the k-th minimum element in an unsorted collection.
+	int kthMinimum(std::vector<int> numbers, int k) {
 
-  // Finds the k-th minimum element in an unsorted collection.
-  int kthMinimum(vector<int> numbers, int k) {
-    // ...
-  }
+		int* arr = new int[k];
+		for (int i = 0; i < k; i++) {
+			arr[i] = numbers[i];
+		}
+
+		std::make_heap(arr, arr + k);
+
+		int sizeNumbers = numbers.size();
+		for (int i = k; i < sizeNumbers; i++) {
+			if (numbers[i] < arr[0]) {
+				arr[0] = numbers[i];
+				std::make_heap(arr, arr + k);
+			}
+		}
+
+		int result = arr[0];
+		
+		delete[] arr;
+
+		return result;
+	}
 };
+
+//int main() {
+//
+//	std::vector<int> numbers = { 9, 5, 11, 2, 4, 7, 8 };
+//	KMin test;
+//	
+//	int k;
+//	std::cout << "k: ";
+//	std::cin >> k;
+//
+//	std::cout << k << "-th smallest: " << test.kthMinimum(numbers, k) << std::endl;
+//	
+//	return 0;
+//}
