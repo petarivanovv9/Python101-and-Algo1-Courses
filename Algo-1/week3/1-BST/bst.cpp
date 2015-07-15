331a79a509887c33b11d387d70a4dc68a1a5b4aa
Petar Ivanov <pepincho95@gmail.com>
2015-07-15 10:01:30 +0300

    add bst week3 task

--- Algo-1/week3/1-BST/bst.cpp
+++ Algo-1/week3/1-BST/bst.cpp
@@ -1,15 +1,41 @@
+#include <iostream>
+
+
 class Node {
 public:
-  int value;
-  Node* left;
-  Node* right;
+	int value;
+	Node* left;
+	Node* right;
 };
 
 class BST {
 public:
 
-  // Checks if a binary tree is a binary search tree.
-  bool isBST(Node* root) {
-    // ...
-  }
+	// Checks if a binary tree is a binary search tree.
+	bool isBST(Node* root) {
+		
+		if (root->value > (root->left)->value && root->value < (root->right)->value && root->right != 0 && root->left != 0) {
+			return (isBST(root->left) && isBST(root->right));
+		}
+		else if (root->left == 0 && root->value < (root->right)->value) {
+			return isBST(root->right);
+		}
+		else if (root->right == 0 && root->value > (root->left)->value) {
+			return isBST(root->left);
+		}
+		else if (root->left == 0 && root->right == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
 };
+
+int main() {
+
+
+
+	return 0;
+}
