c140f91b55dfb909c725dfb4f6e62290f21f3000
Petar Ivanov <pepincho95@gmail.com>
2015-07-19 21:05:02 +0300

    adding quadruplets and phone_book almost ready

--- Algo-1/week2/3-Quadruplets/quadruplets.cpp
+++ Algo-1/week2/3-Quadruplets/quadruplets.cpp
@@ -1,8 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
 class Quadruplets {
 public:
+	// Returns the number of quadruplets that sum to zero
+	int zeroQuadrupletsCount(std::vector<int>& a, std::vector<int>& b, std::vector<int>& c, std::vector<int>& d) {
+		
+		int counter = 0;
 
-  // Returns the number of quadruplets that sum to zero.
-  int zeroQuadrupletsCount(int* a, int* b, int* c, int* d, int size) {
-    // ...
-  }
+		std::vector<int> AandBpermutation;
+
+		for (int i = 0; i < a.size(); i++) {
+			for (int j = 0; j < b.size(); j++) {
+				AandBpermutation.push_back(a[i] + b[j]);
+			}
+		}
+
+		std::sort(AandBpermutation.begin(), AandBpermutation.end());
+
+		for (int i = 0; i < c.size(); i++) {
+			for (int j = 0; j < d.size(); j++) {
+				int sum = -(c[i] + d[j]);
+				counter += std::upper_bound(AandBpermutation.begin(), AandBpermutation.end(), sum) - std::lower_bound(AandBpermutation.begin(), AandBpermutation.end(), sum);
+			}
+		}
+
+		return counter;
+	}
 };
+
+int main() {
+
+	int n, temp;
+	std::cin >> n;
+	std::vector<int> a, b, c, d;
+	
+	Quadruplets test;
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> temp;
+		a.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> temp;
+		b.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> temp;
+		c.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> temp;
+		d.push_back(temp);
+	}
+	std::cout << test.zeroQuadrupletsCount(a, b, c, d) << std::endl;
+	
+	return 0;
+}
--- Algo-1/week2/4-Phone-Book/phone_book.cpp
+++ Algo-1/week2/4-Phone-Book/phone_book.cpp
@@ -1,19 +1,86 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 
-using namespace std;
+//using namespace std;
 
-class Contact {
-public:
-  string name;
-  int number;
-};
+std::vector<std::string> lookUpNames(std::vector<std::pair<int, std::string> > phoneBook, std::vector<int> numbers) {
 
-class PhoneBook {
-public:
+	std::vector<std::string> names;
 
-  // Find the names of people based on their phone numbers.
-  vector<string> lookupNames(vector<Contact> phoneBook, vector<int> numbers) {
-    // ...
-  }
-};
+	std::sort(phoneBook.begin(), phoneBook.end());
+
+	for (size_t i = 0; i < numbers.size(); i++) {
+
+		int left = 0;
+		int right = phoneBook.size() - 1;
+		int mid;
+		bool isNumberFound = false;
+
+		while (left <= right) {
+
+			mid = left + (right - left) / 2;
+
+			if (phoneBook[mid].first == numbers[i]) {
+				isNumberFound = true;
+				break;
+			}
+			else if (phoneBook[mid].first < numbers[i]) {
+				left = mid + 1;
+			}
+			else {
+				right = mid - 1;
+			}
+		}
+
+		if (isNumberFound) {
+			//std::cout << "here" << std::endl;
+			names.push_back(phoneBook[mid].second);
+			//std::cout << names[0] << std::endl;
+		}
+
+	}
+
+	return names;
+}
+
+int main() {
+
+	int n;
+	std::cin >> n;
+
+	int m;
+	std::cin >> m;
+
+	int number;
+	std::string name;
+
+	std::vector<std::pair<int, std::string> > phoneBook(n);
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> number;
+		//std::cin >> name;
+		std::getline(std::cin, name);
+		phoneBook.push_back(std::make_pair(number, name));
+		std::cout << "+++ " << name << std::endl;
+		std::cout << "--- " << phoneBook[i].first << std::endl;
+		std::cout << "--- " << phoneBook[i].second << std::endl;
+	}
+
+	std::vector<int> numbers(m);
+
+	for (int i = 0; i < m; i++) {
+		std::cin >> number;
+		numbers.push_back(number);
+	}
+
+	std::vector<std::string> result = lookUpNames(phoneBook, numbers);
+
+	for (int i = 0; i < m; i++) {
+		//cout << "?/?" << endl;
+		std::cout << result[i] << std::endl;
+	}
+
+	return 0;
+}
