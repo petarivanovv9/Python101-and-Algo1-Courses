55df8d22606b5f12f44a40ee9d5fccce9dc6a313
Petar Ivanov <pepincho95@gmail.com>
2015-09-16 14:49:54 +0300

    fix anagrams.py

--- Tasks from Programming 51/anagrams.py
+++ Tasks from Programming 51/anagrams.py
@@ -1,18 +1,26 @@
-def main():
-    word_1 = input()
-    word_1 = word_1.lower()
-    word_1 = list(word_1)
-    # print(word_1)
-    # word_1.sort()
-    # print(word_1)
-    word_2 = input()
-    word_2 = word_2.lower()
-    word_2 = list(word_2)
-    # print(word_2)
-    # word_2.sort()
-    # print(word_2)
+class Anagrams:
 
-    if word_1 == word_2:
+    @staticmethod
+    def are_anagrams(a, b):
+        a = a.lower()
+        a = list(a)
+        a = sorted(a)
+
+        b = b.lower()
+        b = list(b)
+        b = sorted(b)
+
+        if a is b:
+            return True
+        else:
+            return False
+
+
+def main():
+    word_1 = str(input())
+    word_2 = str(input())
+
+    if Anagrams.are_anagrams(word_1, word_2) is True:
         print("ANAGRAMS")
     else:
         print("NOT ANAGRAMS")
