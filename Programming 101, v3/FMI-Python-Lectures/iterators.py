a6d56b3a6bf13e96d96a41edbb71682e884fed03
Petar Ivanov <pepincho95@gmail.com>
2015-09-23 12:10:32 +0300

    add more info about iterators

--- Programming 101, v3/FMI-Python-Lectures/iterators.py
+++ Programming 101, v3/FMI-Python-Lectures/iterators.py
@@ -20,10 +20,10 @@ interjections = [
     'Joff-tchoff-tchoffo-tchoffo-tchoff!'
 ]
 
-iterator = iter(interjections)
-while True:
-    interjection = next(iterator)
-    print(interjection)
+# iterator = iter(interjections)
+# while True:
+#     interjection = next(iterator)
+#     print(interjection)
 
 iterable = iter([1, 2, 3])
 print(iter(iterable) is iterable)  # True
@@ -59,3 +59,74 @@ it = IterableThingie()
 for i in it:
     print(i)
 # ⰰ, ⰱ, ⰲ, ⰳ, ⰴ, ⰵ, ⰶ, ⰷ, ⰸ, ⰹ, ⰺ, ⰻ
+
+
+print(40 * '-')
+
+
+# Стражари и апаши
+
+counter = 0
+
+
+def clbl():
+    # WRITING CODE LIKE THIS WILL LIKELY RESULT
+    # IN THE AGONIZING UNTIMELY DEATHS OF MANY
+    # CUTE FURRY ANIMALS
+
+    global counter
+    counter += 1
+
+    return counter
+
+iterable = iter(clbl, 23)
+# [1, 2, 3, …, 22]
+for i in iterable:
+    print(i)
+
+
+print(40 * '-')
+
+odd = filter(lambda num: num % 2, range(10))
+print(iter(odd) is odd)  # True
+
+
+print(40 * '-')
+
+
+loud_names = ['JEFF', 'STONE', 'MIKE', 'EDDIE', 'MATT']
+quiet_names = map(lambda name: name.lower(), loud_names)
+loud_names[3] = 'VEDDER'
+
+print(list(quiet_names))
+# ['jeff', 'stone', 'mike', 'vedder', 'matt']
+
+
+print(40 * '-')
+
+numbers = [12, 15, 14, 10, 5, 7, 6]
+print(numbers.sort())  # None
+print(numbers)  # [5, 6, 7, 10, 12, 14, 15]
+
+
+print(40 * '-')
+
+
+numbers = [12, 15, 14, 10, 5, 7, 6]
+print(sorted(numbers))  # [5, 6, 7, 10, 12, 14, 15]
+print(numbers)  # [12, 15, 14, 10, 5, 7, 6]
+
+points = [(10, 3), (4, 8), (5, 9), (2, 3), (12, 6), (7, 4)]
+print(sorted(points))
+# [(2, 3), (4, 8), (5, 9), (7, 4), (10, 3), (12, 6)]
+print(sorted(points, key=lambda point: point[1]))
+# [(10, 3), (2, 3), (7, 4), (12, 6), (4, 8), (5, 9)]
+
+
+print(40 * '-')
+
+
+numbers = [12, 15, 14, 10, 5, 7, 6]
+print(reversed(numbers))
+# <list_reverseiterator object at 0x7f14ff534490>
+print(numbers.reverse())  # None
